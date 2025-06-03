e74e376753115aaa2833ace99cbdc6fd7340a768
timoteo425 <timocavalcante@gmail.com>
2025-06-03 14:44:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+from flask import Flask
 import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Sistema de controle de estoque funcionando!'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
